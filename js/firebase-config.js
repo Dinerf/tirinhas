56de56d9e1fb37c7f91bc600e52f62d17b39883c
Dine <ariadne0288@hotmail.com>
2018-08-12 14:02:36 -0300

    estrutura de arquivos

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyCZX9X4Nsz02j6QnzKtsKxNLXpXxomCKYE",
+  authDomain: "projeto-teste-49d6c.firebaseapp.com",
+  databaseURL: "https://projeto-teste-49d6c.firebaseio.com",
+  projectId: "projeto-teste-49d6c",
+  storageBucket: "projeto-teste-49d6c.appspot.com",
+  messagingSenderId: "434955727160"
+};
+firebase.initializeApp(config);
