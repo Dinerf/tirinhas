89b5a965889bd7ca9873a51d067f843af2fdefc2
Dine <ariadne0288@hotmail.com>
2018-08-15 14:25:50 -0300

    remove função amigos

--- js/index.js
+++ js/index.js
@@ -112,33 +112,3 @@ function createComment(text, key) {
   //     });
   //   });
   // });
-}
-
-// // Amigos
-// var usersList = $('.usersList');
-// var userName = $('.userName');
-
-// $('.userBtn').click(function() {
-//   getUser(userName.val());
-// });
-
-// function getUser(name) {
-//   var data = {
-//     name: name
-//   }
-
-//   return firebase.database().ref().child('users').push(data);
-//   // return database.ref('users/' + USER_ID).push({
-//   //   name: name
-//   // });
-// };
-
-// firebase.database().ref('users').on('value', function(snapshot) {
-//   usersList.innerHTML = '';
-  
-//   snapshot.forEach(function(item) {
-//     var li = document.createElement('li');
-//     li.appendChild(document.createTextNode(item.val().name));
-//     usersList.appendChild(li);
-//   });
-// });
